eec5c6a27354b0c2218d4a1bebe401e286e77325
Derick Hooley <dghooley@gmail.com>
2022-03-12 20:17:40 -0600

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,153 @@
-import './App.css';
+/*import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from 'react-tsparticles';
 import Navbar from './components/Navbar';
+import Header from "./components/Header";
 
-function App() {
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+
+
+
+  function App() {
   return (
-    <Navbar/>
+    <>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
-}
+}  
+
 
 export default App;
+*/
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Particles from "react-particles-js";
+import Particles from "react-tsparticles";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+ 
+function App() {
+  return (
+    <>
+      <Particles
+        className="tsparticles-canvas-el"
+        // params={{
+        //   particles: {
+        //     number: {
+        //       value: 30,
+        //       density: {
+        //         enable: true,
+        //         value_area: 900,
+        //       },
+        //     },
+        //     shape: {
+        //       type: "circle",
+        //       stroke: {
+        //         width: 6,
+        //         color: "#f9ab00",
+        //       },
+        //     },
+        //   },
+        // }}
+        options={{
+          background: {
+            color: {
+              value: "transparent",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#f9ab00",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+              value: 50,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
+}
+ 
+export default App;
+
