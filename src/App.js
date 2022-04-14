006d688a467cc3fbaeb72563450dac9343289f0a
Derick Hooley <dghooley@gmail.com>
2022-04-14 08:58:21 -0500

    Experience component completed

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
+import Experience from "./components/Experience";
 
 function App() {
   return (
@@ -151,6 +152,7 @@ function App() {
       <Header />
       <AboutMe />
       <Services />
+      <Experience />
     </>
   );
 }
