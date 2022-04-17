11a26611d2eff08a52a4475783e086e19c812908
Derick Hooley <dghooley@gmail.com>
2022-04-16 22:58:38 -0500

    Contact Me Started

---
+++ src/components/Contacts.js
@@ -0,0 +1,93 @@
+import React from "react";
+import emailjs from "emailjs-com";
+
+
+const Contacts = () => {
+
+    const serviceID = "service_ID";
+    const templateID = "template_ID";
+    const userID = "jKNk32fiM2Smbsy99";
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('serviceID', 'templateID', e.target, 'userID')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
+    return (
+        <div className="contacts">
+            <div className="text-center">
+                <h1>contact me</h1>
+                <p>Please fill out the form and describe your needs and I'll contact you as soon as possible.</p>
+            </div>
+            <div className="container">
+                <form onSubmit={sendEmail}>
+                <div className="row">
+                    <div className="col-md-6 col-xs-12">
+                        {/* NAME INPUT */}
+                        <div className="text-center">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Name"
+                                name="name"
+                            />
+                            <div className="line"></div>
+                        </div>
+                        {/* PHONE INPUT */}
+                        <div className="text-center">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Phone Number"
+                                name="phone"
+                            />
+                            <div className="line"></div>
+                        </div>
+                        {/* EMAIL INPUT */}
+                        <div className="text-center">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Email"
+                                name="email"
+                            />
+                            <div className="line"></div>
+                        </div>
+                        {/* SUBJECT INPUT */}
+                        <div className="text-center">
+                            <input
+                                text="text"
+                                className="form-control"
+                                placeholder="Subject"
+                                name="subject"
+                            />
+                            <div className="line"></div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-xs-12">
+                        {/* DESCRIPTION */}
+                        <div className="text-center">
+                            <textarea
+                                type="text"
+                                className="form-control"
+                                placeholder="Please describe your project..."
+                                name="description"
+                            ></textarea>
+                            <div className="line"></div>
+                        </div>
+                        <button className="btn-main-offer contact-btn" type="submit">contact me</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Contacts;
